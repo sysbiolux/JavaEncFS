548b949876a976087187ff81debaa6d2489d80a5
Thomas Pfau <thomas.pfau@uni.lu>
2018-09-19 09:35:07 +0200

    Fix image -> images

--- makeMacApp.sh
+++ makeMacApp.sh
@@ -7,7 +7,7 @@ mkdir JavaEncFS.app/Contents/Ressources
 mkdir JavaEncFS.app/Contents/Ressources/Java
 cp info.plist JavaEncFS.app/Contents
 cp /System/Library/Frameworks/JavaVM.framework/Versions/Current/Resources/MacOS/JavaApplicationStub JavaEncFS.app/Contents/MacOS
-cp src/main/image/JavaEncFS.icns JavaEncFS.app/Contents/Ressources
+cp src/main/images/JavaEncFS.icns JavaEncFS.app/Contents/Ressources
 echo APPL???? >> JavaEncFS.app/Contents/PkgInfo
 cp target/encFS-1.0-jar-with-dependencies.jar JavaEncFS.app/Contents/Ressources/Java/JavaEncFS.jar
 /Developer/Tools/SetFile -a B JavaEncFS.app.app
