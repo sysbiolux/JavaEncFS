80139833f79cbb2634685a21b21a26533d239643
Thomas Pfau <thomas.pfau@uni.lu>
2018-09-19 10:45:34 +0200

    Updated Mac App

---
+++ Java Encfs.app/Contents/MacOS/JavaEncFS.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export PATH=/usr/local/bin/:$PATH
+java -jar JavaEncFS.jar
+
--- makeMacApp.sh
+++ makeMacApp.sh
@@ -1,13 +1,2 @@
 #!/usr/sh
-
-mkdir JavaEncFS.app
-mkdir JavaEncFS.app/Contents
-mkdir JavaEncFS.app/Contents/MacOS
-mkdir JavaEncFS.app/Contents/Ressources
-mkdir JavaEncFS.app/Contents/Ressources/Java
-cp info.plist JavaEncFS.app/Contents
-cp /System/Library/Frameworks/JavaVM.framework/Versions/Current/Resources/MacOS/JavaApplicationStub JavaEncFS.app/Contents/MacOS
-cp src/main/images/JavaEncFS.icns JavaEncFS.app/Contents/Ressources
-echo APPL???? >> JavaEncFS.app/Contents/PkgInfo
-cp target/encFS-1.0-jar-with-dependencies.jar JavaEncFS.app/Contents/Ressources/Java/JavaEncFS.jar
-SetFile -a B JavaEncFS.app
+cp target/encFS-1.0-jar-with-dependencies.jar JavaEncFS.app/Contents/MacOS/JavaEncFS.jar
